2790c360592ce217c439647a1c0b5b12e23996b9
JoshMiller5994 <talk2joshbgosh@gmail.com>
2021-01-12 10:59:04 -0700

    added the snapshot

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,32 +1,29 @@
 export function pluralize(name, count) {
   if (count === 1) {
-    return name
+    return name;
   }
-  return name + 's'
+  return name + 's';
 }
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    const request - window.indexedDB.open('shop-shoppe', 1);
-
+    const request = window.indexedDB.open('shop-shop', 1);
     let db, tx, store;
-
-    request.onupgradeneeded = function (e) {
+    request.onupgradeneeded = function(e) {
       const db = request.result;
-
       db.createObjectStore('products', { keyPath: '_id' });
       db.createObjectStore('categories', { keyPath: '_id' });
       db.createObjectStore('cart', { keyPath: '_id' });
-    }
+    };
 
     request.onerror = function(e) {
       console.log('There was an error');
-    }
+    };
 
     request.onsuccess = function(e) {
       db = request.result;
       tx = db.transaction(storeName, 'readwrite');
-      store = tx.objectStore(storename);
+      store = tx.objectStore(storeName);
 
       db.onerror = function(e) {
         console.log('error', e);
@@ -51,9 +48,9 @@ export function idbPromise(storeName, method, object) {
           break;
       }
 
-      db.oncomplete = function() {
+      tx.oncomplete = function() {
         db.close();
       };
     };
   });
-};
+}
