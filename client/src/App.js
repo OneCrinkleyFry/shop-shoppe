44989664cad9079edc6c5349f2a820fe4f705c6a
JoshMiller5994 <talk2joshbgosh@gmail.com>
2021-01-13 09:36:47 -0700

    added the code snapshot

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import Success from './pages/Success';
 import { StoreProvider } from "./utils/GlobalState";
+import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
@@ -36,9 +36,9 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,20 +1,27 @@
 import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from '@apollo/react-hooks';
+import { QUERY_CHECKOUT } from "../../utils/queries"
 import { idbPromise } from "../../utils/helpers"
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import { QUERY_CHECKOUT } from '../../utils/queries';
-import { loadStripe } from '@stripe/stripe-js';
 import "./style.css";
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
-
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session })
+      })
+    }
+  }, [data]);
 
   useEffect(() => {
     async function getCart() {
@@ -26,14 +33,6 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-
-  useEffect(() => {
-    if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
-    }
-  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -50,7 +49,7 @@ const Cart = () => {
   function submitCheckout() {
     const productIds = [];
 
-    state.cart.forEach(item => {
+    state.cart.forEach((item) => {
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
       }
